68b4ab0efa0501c872ca01744b401d6657781662
Peter Wang <novalazy@gmail.com>
2019-02-04 15:48:28 +1100

    Imported mercury-srcdist-rotd-2018-11-07

---
+++ tests/invalid/cyclic_typeclass_2.m
@@ -0,0 +1,20 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+
+:- module cyclic_typeclass_2.
+
+% This test is a cut down version of cyclic_typeclass. This one doesn't
+% cause the compiler to go into an infinite loop, but it still contains
+% an error that goes unreported, and may cause an infinite loop when
+% compiling other modules that import it.
+
+:- interface.
+
+:- typeclass foo(A) <= bar(A) where [
+    func foo(A) = int
+].
+
+:- typeclass bar(A) <= foo(A) where [
+    func bar(A) = int
+].
