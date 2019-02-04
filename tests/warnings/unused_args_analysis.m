b0c626920e05ced51b82b38bc87f4789f16e1fe0
Peter Wang <novalazy@gmail.com>
2019-02-04 15:48:20 +1100

    Imported mercury-srcdist-rotd-2018-10-26

---
+++ tests/warnings/unused_args_analysis.m
@@ -0,0 +1,16 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+
+:- module unused_args_analysis.
+
+:- interface.
+
+:- pred p(int::in, int::out) is det.
+
+:- implementation.
+
+:- import_module unused_args_analysis2.
+
+p(X, Y) :-
+    p2(X, Y).
