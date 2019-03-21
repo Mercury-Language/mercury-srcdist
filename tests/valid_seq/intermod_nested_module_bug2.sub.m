7970a735d8bdcad8ad3e183fde33326377390f7a
Julien Fischer <juliensf@gmail.com>
2019-03-21 12:45:06 +0000

    Imported mercury-srcdist-rotd-2019-03-21

---
+++ tests/valid_seq/intermod_nested_module_bug2.sub.m
@@ -0,0 +1,18 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+
+:- module intermod_nested_module_bug2__sub.
+
+:- interface.
+
+:- type post == assoc_list(string).
+
+:- pred read_post(cgi, maybe_error(post), io, io).
+:- mode read_post(in, out, di, uo) is det.
+
+:- implementation.
+
+read_post(_CGI, error("foo")) --> [].
+
+:- end_module intermod_nested_module_bug2__sub.
