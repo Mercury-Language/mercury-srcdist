0bad7d919fba14bf9ee21fe2e7c85e522545b4fe
Julien Fischer <juliensf@gmail.com>
2019-05-21 04:10:53 +0000

    Imported mercury-srcdist-rotd-2018-03-28

---
+++ tests/declarative_debugger/trust_2.m
@@ -0,0 +1,17 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+
+:- module trust_2.
+
+:- interface.
+
+:- import_module trust_1.
+
+:- pred concat(w::in, w::in, w::out) is cc_multi.
+
+:- implementation.
+
+:- import_module string.
+
+concat(w(S), w(T), w(S++T)).
