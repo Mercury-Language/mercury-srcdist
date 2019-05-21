b153d04488d0881295bfbe47b098e1571ba429a5
Julien Fischer <juliensf@gmail.com>
2019-05-21 04:11:50 +0000

    Imported mercury-srcdist-rotd-2018-04-02

---
+++ tests/declarative_debugger/typed_unify.m
@@ -0,0 +1,24 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+
+:- module typed_unify.
+
+:- interface.
+
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+
+:- import_module univ.
+
+main(!IO) :-
+    U = univ(1),
+    ( type_to_univ(I, U) ->
+        io.write_int(I, !IO)
+    ;
+        true
+    ),
+    nl(!IO).
