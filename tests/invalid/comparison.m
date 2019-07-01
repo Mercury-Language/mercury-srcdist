a91c6c51aa0958e58cd328a99887a9947c9679ed
Julien Fischer <juliensf@gmail.com>
2019-07-01 22:13:11 +1000

    Imported mercury-srcdist-rotd-2019-07-01

---
+++ tests/invalid/comparison.m
@@ -0,0 +1,24 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+
+:- module comparison.
+:- interface.
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+:- import_module int.
+
+main(!IO) :-
+    ( test(1.0, 2.0) ->
+        io.write_string("ok\n", !IO)
+    ;
+        io.write_string("not ok\n", !IO)
+    ).
+
+:- pred test(float::in, float::in) is semidet.
+
+test(A, B) :-
+    A < B.
