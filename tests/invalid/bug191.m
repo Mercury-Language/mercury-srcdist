17d52c86c00d1844ffc9bc10e779de5e99400e5c
Julien Fischer <juliensf@gmail.com>
2019-07-09 22:47:11 +1000

    Imported mercury-srcdist-rotd-2019-07-09

---
+++ tests/invalid/bug191.m
@@ -0,0 +1,32 @@
+% Regression test for bug #191:
+% This program caused the following compiler abort in rotd-2011-03-22:
+%
+%    Software Error: code_gen.m: Unexpected: semidet model in det context
+%
+:- module bug191.
+:- interface.
+
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+
+:- type foo ---> a(int); b(string).
+
+:- inst a ---> a(ground).
+
+:- typeclass foo(T) where [pred baz(T::in, int::in, foo::out(a)) is det].
+:- instance foo(int) where [pred(baz/3) is bar].
+
+:- pred foo(foo::in(a), int::out) is det.
+foo(a(I), I).
+
+% should be bar(int::in, foo::out(a))
+:- pred bar(int::in, int::in, foo::out) is det.
+bar(_, _S, b("GOTCHA")).
+
+main(!IO) :-
+    baz(561, 42, F),
+    foo(F, I),
+    print(I, !IO), nl(!IO).
