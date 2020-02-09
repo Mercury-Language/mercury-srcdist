34844b21707be2998dd7cb98654bd22a63fb3537
Julien Fischer <juliensf@gmail.com>
2020-02-09 23:54:17 +1100

    Imported mercury-srcdist-rotd-2020-02-09

---
+++ compiler/ll_backend.delay_slot.c
@@ -0,0 +1,295 @@
+/*
+** Automatically generated from `delay_slot.m'
+** by the Mercury compiler,
+** version rotd-2020-02-09
+** configured for x86_64-pc-linux-gnu.
+** Do not edit.
+**
+** The autoconfigured grade settings governing
+** the generation of this C file were
+**
+** TAG_BITS=2
+** UNBOXED_FLOAT=no
+** UNBOXED_INT64S=no
+** PREGENERATED_DIST=yes
+** HIGHLEVEL_CODE=yes
+**
+** END_OF_C_GRADE_INFO
+*/
+
+
+// :- module ll_backend.delay_slot.
+// :- implementation.
+
+/*
+INIT mercury__ll_backend__delay_slot__init
+ENDINIT
+*/
+
+#include "ll_backend.delay_slot.mih"
+
+
+#include "backend_libs.mih"
+#include "check_hlds.mih"
+#include "hlds.mih"
+#include "libs.mih"
+#include "ll_backend.mih"
+#include "mdbcomp.mih"
+#include "parse_tree.mih"
+#include "backend_libs.builtin_ops.mih"
+#include "backend_libs.rtti.mih"
+#include "check_hlds.type_util.mih"
+#include "hlds.code_model.mih"
+#include "hlds.hlds_data.mih"
+#include "hlds.hlds_llds.mih"
+#include "hlds.hlds_module.mih"
+#include "hlds.hlds_pred.mih"
+#include "libs.globals.mih"
+#include "ll_backend.layout.mih"
+#include "ll_backend.llds.mih"
+#include "mdbcomp.goal_path.mih"
+#include "mdbcomp.prim_data.mih"
+#include "mdbcomp.program_representation.mih"
+#include "mdbcomp.sym_name.mih"
+#include "array.mih"
+#include "assoc_list.mih"
+#include "bimap.mih"
+#include "bool.mih"
+#include "builtin.mih"
+#include "char.mih"
+#include "construct.mih"
+#include "cord.mih"
+#include "counter.mih"
+#include "deconstruct.mih"
+#include "enum.mih"
+#include "list.mih"
+#include "map.mih"
+#include "maybe.mih"
+#include "multi_map.mih"
+#include "ops.mih"
+#include "pair.mih"
+#include "pretty_printer.mih"
+#include "private_builtin.mih"
+#include "set.mih"
+#include "set_ordlist.mih"
+#include "string.mih"
+#include "term.mih"
+#include "tree234.mih"
+#include "type_desc.mih"
+#include "unit.mih"
+#include "univ.mih"
+#include "varset.mih"
+#include "parse_tree.prog_data.mih"
+#include "parse_tree.prog_data_foreign.mih"
+#include "parse_tree.prog_data_pragma.mih"
+#include "parse_tree.prog_foreign.mih"
+#include "parse_tree.prog_item.mih"
+#include "parse_tree.set_of_var.mih"
+
+
+
+
+
+static /* final */ const MR_Box ll_backend__delay_slot_scalar_common_1[1][1];
+
+static /* final */ const MR_Box ll_backend__delay_slot_scalar_common_2[1][2];
+
+static /* final */ const MR_Box ll_backend__delay_slot_scalar_common_3[1][3];
+
+
+
+
+static /* final */ const MR_Box ll_backend__delay_slot_scalar_common_1[1][1] = {
+  /* row 0 */
+  {
+    ((MR_Box) ((MR_Unsigned) 0U))
+  },
+};
+
+static /* final */ const MR_Box ll_backend__delay_slot_scalar_common_2[1][2] = {
+  /* row 0 */
+  {
+    ((MR_Box) (MR_Word) ((MR_Unsigned) 0U)),
+    ((MR_Box) ((MR_Integer) 0))
+  },
+};
+
+static /* final */ const MR_Box ll_backend__delay_slot_scalar_common_3[1][3] = {
+  /* row 0 */
+  {
+    ((MR_Box) (MR_Word) ((MR_Unsigned) 1U)),
+    ((MR_Box) (MR_mkword(MR_mktag(3), &ll_backend__delay_slot_scalar_common_2[0]))),
+    ((MR_Box) (&ll_backend__delay_slot_scalar_common_1[0]))
+  },
+};
+
+
+
+#include "array.mh"
+#include "string.mh"
+
+
+
+void MR_CALL 
+ll_backend__delay_slot__fill_branch_delay_slot_2_p_0(
+  MR_Word HeadVar__1_1,
+  MR_Word * HeadVar__2_2)
+{
+  {
+    MR_bool succeeded;
+
+    if ((HeadVar__1_1 == (MR_Word) ((MR_Unsigned) 0U)))
+      *HeadVar__2_2 = (MR_Word) ((MR_Unsigned) 0U);
+    else
+    {
+      MR_Word Instr0_3 = ((MR_Word) ((MR_hl_field(MR_mktag(1), HeadVar__1_1, (MR_Integer) 0))));
+      MR_Word Instrs0_4 = ((MR_Word) ((MR_hl_field(MR_mktag(1), HeadVar__1_1, (MR_Integer) 1))));
+      MR_Word Instr1_8;
+      MR_Word Instr2_9;
+      MR_Word Tail0_11;
+      MR_String C2_19;
+      MR_Word Instr3_10;
+      MR_Integer Size_15;
+      MR_Word Var_24 = ((MR_Word) ((MR_hl_field(MR_mktag(0), Instr0_3, (MR_Integer) 0))));
+      MR_Word Var_25;
+      MR_Word Var_26;
+      MR_Word Var_27;
+      MR_Word Var_28;
+      MR_Word Var_29;
+      MR_Word Var_30;
+      MR_Word Var_31;
+      MR_Word Var_32;
+      MR_Integer Var_42;
+
+      succeeded = ((((MR_tag((MR_Word) Var_24)) == (MR_Integer) 3)) && ((((MR_Integer) ((MR_hl_field(MR_mktag(3), Var_24, (MR_Integer) 0)))) == (MR_Integer) 5)));
+      if (succeeded)
+      {
+        succeeded = (Instrs0_4 != (MR_Word) ((MR_Unsigned) 0U));
+        if (succeeded)
+        {
+          Instr1_8 = ((MR_Word) ((MR_hl_field(MR_mktag(1), Instrs0_4, (MR_Integer) 0))));
+          Var_25 = ((MR_Word) ((MR_hl_field(MR_mktag(1), Instrs0_4, (MR_Integer) 1))));
+          succeeded = (Var_25 != (MR_Word) ((MR_Unsigned) 0U));
+          if (succeeded)
+          {
+            Instr2_9 = ((MR_Word) ((MR_hl_field(MR_mktag(1), Var_25, (MR_Integer) 0))));
+            Var_26 = ((MR_Word) ((MR_hl_field(MR_mktag(1), Var_25, (MR_Integer) 1))));
+            succeeded = (Var_26 != (MR_Word) ((MR_Unsigned) 0U));
+            if (succeeded)
+            {
+              Instr3_10 = ((MR_Word) ((MR_hl_field(MR_mktag(1), Var_26, (MR_Integer) 0))));
+              Tail0_11 = ((MR_Word) ((MR_hl_field(MR_mktag(1), Var_26, (MR_Integer) 1))));
+              Var_27 = ((MR_Word) ((MR_hl_field(MR_mktag(0), Instr1_8, (MR_Integer) 0))));
+              succeeded = ((((MR_tag((MR_Word) Var_27)) == (MR_Integer) 3)) && ((((MR_Integer) ((MR_hl_field(MR_mktag(3), Var_27, (MR_Integer) 0)))) == (MR_Integer) 9)));
+              if (succeeded)
+              {
+                Var_28 = ((MR_Word) ((MR_hl_field(MR_mktag(0), Instr2_9, (MR_Integer) 0))));
+                succeeded = ((((MR_tag((MR_Word) Var_28)) == (MR_Integer) 3)) && ((((MR_Integer) ((MR_hl_field(MR_mktag(3), Var_28, (MR_Integer) 0)))) == (MR_Integer) 24)));
+                if (succeeded)
+                {
+                  Size_15 = ((MR_Integer) ((MR_hl_field(MR_mktag(3), Var_28, (MR_Integer) 1))));
+                  Var_29 = ((MR_Word) ((MR_hl_field(MR_mktag(0), Instr3_10, (MR_Integer) 0))));
+                  C2_19 = ((MR_String) ((MR_hl_field(MR_mktag(0), Instr3_10, (MR_Integer) 1))));
+                  succeeded = ((((MR_tag((MR_Word) Var_29)) == (MR_Integer) 3)) && ((((MR_Integer) ((MR_hl_field(MR_mktag(3), Var_29, (MR_Integer) 0)))) == (MR_Integer) 1)));
+                  if (succeeded)
+                  {
+                    Var_30 = ((MR_Word) ((MR_hl_field(MR_mktag(3), Var_29, (MR_Integer) 1))));
+                    Var_31 = ((MR_Word) ((MR_hl_field(MR_mktag(3), Var_29, (MR_Integer) 2))));
+                    succeeded = ((((MR_tag((MR_Word) Var_30)) == (MR_Integer) 3)) && ((((MR_Integer) ((MR_hl_field(MR_mktag(3), Var_30, (MR_Integer) 0)))) == (MR_Integer) 0)));
+                    if (succeeded)
+                    {
+                      Var_42 = ((MR_Integer) ((MR_hl_field(MR_mktag(3), Var_30, (MR_Integer) 1))));
+                      succeeded = (Size_15 == Var_42);
+                      if (succeeded)
+                      {
+                        succeeded = ((MR_tag((MR_Word) Var_31)) == (MR_Integer) 0);
+                        if (succeeded)
+                        {
+                          Var_32 = ((MR_Word) ((MR_hl_field(MR_mktag(0), Var_31, (MR_Integer) 0))));
+                          succeeded = (Var_32 == (MR_Word) ((MR_Unsigned) 0U));
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+      if (succeeded)
+      {
+        MR_Word Tail1_20;
+        MR_String NewC2_21;
+        MR_Word EarlySave_22;
+        MR_Word Var_39;
+        MR_Word Var_40;
+        MR_Word Var_41;
+
+        ll_backend__delay_slot__fill_branch_delay_slot_2_p_0(Tail0_11, &Tail1_20);
+        mercury__string__append_3_p_2(C2_19, (MR_String) " (early save in delay slot)", &NewC2_21);
+        {
+          EarlySave_22 = (MR_Word) MR_new_object(MR_Word, (2 * sizeof(MR_Word)), NULL, NULL);
+          MR_hl_field(MR_mktag(0), EarlySave_22, 0) = ((MR_Box) (MR_mkword(MR_mktag(3), &ll_backend__delay_slot_scalar_common_3[0])));
+          MR_hl_field(MR_mktag(0), EarlySave_22, 1) = ((MR_Box) (NewC2_21));
+        }
+        {
+          Var_41 = (MR_Word) MR_mkword(MR_mktag(1), MR_new_object(MR_Word, (2 * sizeof(MR_Word)), NULL, NULL));
+          MR_hl_field(MR_mktag(1), Var_41, 0) = ((MR_Box) (Instr2_9));
+          MR_hl_field(MR_mktag(1), Var_41, 1) = ((MR_Box) (Tail1_20));
+        }
+        {
+          Var_40 = (MR_Word) MR_mkword(MR_mktag(1), MR_new_object(MR_Word, (2 * sizeof(MR_Word)), NULL, NULL));
+          MR_hl_field(MR_mktag(1), Var_40, 0) = ((MR_Box) (Instr1_8));
+          MR_hl_field(MR_mktag(1), Var_40, 1) = ((MR_Box) (Var_41));
+        }
+        {
+          Var_39 = (MR_Word) MR_mkword(MR_mktag(1), MR_new_object(MR_Word, (2 * sizeof(MR_Word)), NULL, NULL));
+          MR_hl_field(MR_mktag(1), Var_39, 0) = ((MR_Box) (EarlySave_22));
+          MR_hl_field(MR_mktag(1), Var_39, 1) = ((MR_Box) (Var_40));
+        }
+        {
+          MR_Word base;
+          base = (MR_Word) MR_mkword(MR_mktag(1), MR_new_object(MR_Word, (2 * sizeof(MR_Word)), NULL, NULL));
+          *HeadVar__2_2 = base;
+          MR_hl_field(MR_mktag(1), base, 0) = ((MR_Box) (Instr0_3));
+          MR_hl_field(MR_mktag(1), base, 1) = ((MR_Box) (Var_39));
+        }
+      }
+      else
+      {
+        MR_Word Instrs1_23;
+
+        ll_backend__delay_slot__fill_branch_delay_slot_2_p_0(Instrs0_4, &Instrs1_23);
+        {
+          MR_Word base;
+          base = (MR_Word) MR_mkword(MR_mktag(1), MR_new_object(MR_Word, (2 * sizeof(MR_Word)), NULL, NULL));
+          *HeadVar__2_2 = base;
+          MR_hl_field(MR_mktag(1), base, 0) = ((MR_Box) (Instr0_3));
+          MR_hl_field(MR_mktag(1), base, 1) = ((MR_Box) (Instrs1_23));
+        }
+      }
+    }
+  }
+}
+
+void mercury__ll_backend__delay_slot__init(void)
+{
+}
+
+void mercury__ll_backend__delay_slot__init_type_tables(void)
+{
+}
+
+void mercury__ll_backend__delay_slot__init_debugger(void)
+{
+	MR_fatal_error("debugger initialization in MLDS grade");
+}
+
+// Ensure everything is compiled with the same grade.
+const char *mercury__ll_backend__delay_slot__grade_check(void)
+{
+    return &MR_GRADE_VAR;
+}
+
+// :- end_module ll_backend.delay_slot.
---
+++ tests/debugger/browser_test.m
@@ -0,0 +1,117 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+
+:- module browser_test.
+:- interface.
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+
+:- import_module list.
+
+:- type big
+    --->    big(big, int, big)
+    ;       small
+    ;       seq(int, list(big), int).
+
+main(!IO) :-
+    % In case we have these files lying around.
+    io.remove_file("browser_test.save.1", _, !IO),
+    io.remove_file("browser_test.save.2", _, !IO),
+    big_data(Data),
+    io.print(Data, !IO),
+    io.write_string(".\n", !IO),
+    list_data(List),
+    io.print(List, !IO),
+    io.write_string(".\n", !IO),
+    print_file("browser_test.save.1", !IO),
+    print_file("browser_test.save.2", !IO),
+    % Clean up after the test.
+    io.remove_file("browser_test.save.1", _, !IO),
+    io.remove_file("browser_test.save.2", _, !IO),
+    a_func(Data) = Data2,
+    write(Data2, !IO),
+    nl(!IO).
+
+:- pred big_data(big::out) is det.
+
+big_data(Data) :-
+    Data = big(
+        big(
+            big(
+                small,
+                1,
+                small
+            ),
+            2,
+            small
+        ),
+        3,
+        big(
+            big(
+                small,
+                4,
+                big(
+                    small,
+                    5,
+                    small
+                )
+            ),
+            6,
+            small
+        )
+    ).
+
+:- pred list_data(big::out) is det.
+
+list_data(Data) :-
+    Element1 = big(
+            big(
+                small,
+                1,
+                small
+            ),
+            2,
+            small
+        ),
+    Element2 = small,
+    Element3 = big(
+            small,
+            4,
+            big(
+                small,
+                5,
+                small
+            )
+        ),
+    Data = seq(1, [Element1, Element2, Element3], 5).
+
+:- pred print_file(string::in, io::di, io::uo) is det.
+
+print_file(FileName, !IO) :-
+    io.open_input(FileName, OpenRes, !IO),
+    (
+        OpenRes = ok(Stream),
+        io.read_file_as_string(Stream, ReadRes, !IO),
+        (
+            ReadRes = ok(Contents),
+            io.write_string(FileName, !IO),
+            io.write_string(":\n", !IO),
+            io.write_string(Contents, !IO),
+            io.write_string("\n", !IO)
+        ;
+            ReadRes = error(_, _),
+            io.write_string("read failed\n", !IO)
+        )
+    ;
+        OpenRes = error(_),
+        io.write_string("open failed\n", !IO)
+    ).
+
+:- func a_func(big) = big.
+
+a_func(_) = Big :-
+    big_data(Big).
