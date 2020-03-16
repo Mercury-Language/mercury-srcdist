b6e381412a2792cc0d692936545f4a9071633c8f
Julien Fischer <juliensf@gmail.com>
2020-03-16 22:44:54 +1100

    Imported mercury-srcdist-rotd-2020-03-16

---
+++ tests/hard_coded/func_and_pred.m
@@ -0,0 +1,39 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+% Small test of functions and predicates that have the same name.
+%----------------------------------------------------------------------------
+
+:- module func_and_pred.
+
+:- interface.
+
+:- import_module list.
+:- import_module io.
+
+:- func concat(list(X), list(X)) = list(X).
+:- mode concat(in, in) = out is det.
+
+:- pred concat(list(X), list(X), list(X)).
+:- mode concat(in, in, out) is det.
+
+:- pred main(io__state::di, io__state::uo) is det.
+
+:- implementation.
+
+:- import_module string.
+
+concat([], L) = L.
+concat([E | R], L) = [E | concat(R, L)].
+
+concat([], L, L).
+concat([E | R], L, [E | Z]) :-
+    concat(R, L, Z).
+
+main -->
+    { concat(['H', 'e'], ['l', 'l', 'o', ' '], Hello) },
+    { World = concat(['w', 'o'], ['r', 'l', 'd', '!', '\n']) },
+    { string__from_char_list(concat(Hello, World), HelloWorld) },
+    io__write_string(HelloWorld).
+
+%-----------------------------
---
+++ tests/hard_coded/string_suffix_bug.m
@@ -0,0 +1,20 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+
+:- module string_suffix_bug.
+:- interface.
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+
+:- import_module string.
+
+main -->
+    ( { string__suffix("testing string__suffix", "suffix") } ->
+        io__write_string("yes\n")
+    ;
+        io__write_string("no\n")
+    ).
