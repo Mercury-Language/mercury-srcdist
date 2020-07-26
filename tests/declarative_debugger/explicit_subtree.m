9279c58dd4eadb0fbaf94587b38ebed1715aa36f
Julien Fischer <juliensf@gmail.com>
2020-07-26 23:32:47 +1000

    Imported mercury-srcdist-rotd-2020-07-26

---
+++ tests/declarative_debugger/explicit_subtree.m
@@ -0,0 +1,74 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+%
+% Test the tracking of a subterm through an explicit supertree.
+
+:- module explicit_subtree.
+
+:- interface.
+
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+
+:- import_module exception.
+:- import_module int.
+
+main(!IO) :-
+    p1(10, Q),
+    write_int(Q, !IO),
+    nl(!IO).
+
+:- pred p1(int::in, int::out) is det.
+:- pred p2(int::in, int::out) is det.
+:- pred p3(int::in, int::out) is det.
+
+p1(X, Y) :- p2(X, Y).
+p2(X, Y) :- p3(X, Y).
+p3(X, Y) :- calc(X, Y).
+
+:- pred calc(int::in, int::out) is det.
+
+calc(X, Y) :-
+    (
+        X > 0
+    ->
+        a(Z)
+    ;
+        b(Z)
+    ),
+    divide2(X, Z, Y).
+
+:- pred divide2(int::in, int::in, int::out) is det.
+
+divide2(N, D, Q) :-
+    (
+        D = 0
+    ->
+        throw("zero denominator")
+    ;
+        Q = N // D
+    ).
+
+:- pred b(int::out) is det.
+
+b(-1).
+
+:- pred a(int::out) is det.
+
+a(X + Y - 100) :-
+    q(49, 0, X), q(51, 0, Y).
+
+:- pred q(int::in, int::in, int::out) is det.
+
+q(A, B, C) :-
+    (
+        A =< 0
+    ->
+        B = C
+    ;
+        q(A-1, B+1, C)
+    ).
