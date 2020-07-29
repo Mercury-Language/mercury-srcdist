db050853cef9a3561f42a259af11d0e7993bd35b
Julien Fischer <juliensf@gmail.com>
2020-07-29 22:16:26 +1000

    Imported mercury-srcdist-rotd-2020-07-29

---
+++ tests/hard_coded/existential_reordering.m
@@ -0,0 +1,35 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+%
+% This module tests the use of existential types,
+% including type inference,
+% but not including type class constraints.
+
+:- module existential_reordering.
+:- interface.
+
+:- some [T] func my_exist_t = T.
+
+:- import_module io.
+
+:- pred main(io__state::di, state::uo) is det.
+
+:- implementation.
+:- import_module list.
+:- import_module univ.
+
+main -->
+    % do something which requires knowing the type of L
+    { L = [] },
+    { Univ = univ(L) },
+    write(Univ),
+    nl,
+
+    % now do something which binds the type of L
+    { same_type(L, [my_exist_t]) }.
+
+:- pred same_type(T::unused, T::unused) is det.
+same_type(_, _).
+
+my_exist_t = 42.
