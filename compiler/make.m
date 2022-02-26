3ae3990fcb88b3c52f3318cedcab387556fcaf1c
Julien Fischer <juliensf@gmail.com>
2022-02-26 23:11:51 +1100

    Imported mercury-srcdist-rotd-2022-02-26

---
+++ compiler/make.m
@@ -0,0 +1,40 @@
+%---------------------------------------------------------------------------%
+% vim: ft=mercury ts=4 sw=4 et
+%---------------------------------------------------------------------------%
+% Copyright (C) 2002-2012 The University of Melbourne.
+% Copyright (C) 2013-2021 The Mercury team.
+% This file may only be copied under the terms of the GNU General
+% Public License - see the file COPYING in the Mercury distribution.
+%---------------------------------------------------------------------------%
+%
+% File: make.m.
+% Main author: stayl.
+%
+% A package for "mmc --make", a builtin Mercury-specific make replacement.
+%
+%---------------------------------------------------------------------------%
+
+:- module make.
+:- interface.
+
+:- include_module build.
+:- include_module module_dep_file.
+:- include_module options_file.
+:- include_module top_level.
+
+%---------------------------------------------------------------------------%
+%---------------------------------------------------------------------------%
+
+:- implementation.
+
+:- include_module dependencies.
+:- include_module deps_set.
+:- include_module make_info.
+:- include_module module_target.
+:- include_module program_target.
+:- include_module track_flags.
+:- include_module util.
+
+%---------------------------------------------------------------------------%
+:- end_module make.
+%---------------------------------------------------------------------------%
