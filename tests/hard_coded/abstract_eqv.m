699195cadd28393f0bfb892d244d1320e6c41f45
Julien Fischer <juliensf@gmail.com>
2022-03-24 23:13:45 +1100

    Imported mercury-srcdist-rotd-2022-03-24

---
+++ tests/hard_coded/abstract_eqv.m
@@ -0,0 +1,33 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+
+:- module abstract_eqv.
+
+:- interface.
+
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+
+:- import_module abstract_eqv_1.
+
+main(!IO) :-
+    test(val1, val2, !IO),
+    test(val1, val3, !IO),
+    test(val2, val3, !IO).
+
+:- pred test(t_abs::in, t_abs::in, io::di, io::uo) is det.
+
+test(A, B, !IO) :-
+    io.write(A, !IO),
+    io.write_string(" = ", !IO),
+    io.write(B, !IO),
+    io.write_string(": ", !IO),
+    ( if A = B then
+        io.write_string("true\n", !IO)
+    else
+        io.write_string("false\n", !IO)
+    ).
