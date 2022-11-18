1c0100c21d09a4c956c4333a208c738a44231f9d
Julien Fischer <juliensf@gmail.com>
2022-11-18 23:25:33 +1100

    Imported mercury-srcdist-rotd-2022-11-18

---
+++ tests/invalid/ext_type.m
@@ -0,0 +1,27 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+%
+:- module ext_type.
+
+:- interface.
+:- import_module bool.
+
+:- some [PT] pred p(bool, PT).
+:- mode p(in, out) is det.
+
+:- pred r is semidet.
+
+:- implementation.
+
+p(yes, Y) :-
+    Y = q(42).
+p(no, Z) :-
+    Z = q(43).
+
+r :-
+    q(1) = q(2).
+
+:- some [QT] func q(int) = QT.
+
+q(X) = X.
