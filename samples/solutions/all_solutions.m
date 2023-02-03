3ea1e519cca27469cdab8bb27a711b795cbbac68
Julien Fischer <juliensf@gmail.com>
2023-02-03 22:58:37 +1100

    Imported mercury-srcdist-rotd-2023-02-03

---
+++ samples/solutions/all_solutions.m
@@ -0,0 +1,23 @@
+% An example program to illustrate the use of all-solutions predicates
+% in Mercury.  This program just prints out all solutions to the
+% predicate hello/1.
+
+% This source file is hereby placed in the public domain.  -fjh (the author).
+
+:- module all_solutions.
+:- interface.
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+:- import_module solutions.
+
+main(!IO) :-
+    solutions(hello, List),
+    io.write_strings(List, !IO).
+
+:- pred hello(string::out) is multi.
+
+hello("Hello, world\n").
+hello("Hello again, world\n").
