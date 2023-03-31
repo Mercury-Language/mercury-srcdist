968386688991f055292af22bbba12e6d53e92c60
Julien Fischer <juliensf@gmail.com>
2023-03-31 23:24:03 +1100

    Imported mercury-srcdist-rotd-2023-03-31

---
+++ java/runtime/Constants.java
@@ -0,0 +1,18 @@
+// vim: ts=4 sw=4 expandtab ft=java
+//
+// java/runtime/Constants.java.  Generated from Constants.java.in by configure.
+//
+// Copyright (C) 2001-2004 The University of Melbourne.
+// Copyright (C) 2018 The Mercury team.
+// This file is distributed under the terms specified in COPYING.LIB.
+//
+// This class is used to store miscellaneous Mercury-related constants.
+// At the moment it just stores the library version.
+//
+
+package jmercury.runtime;
+
+public class Constants {
+    public static final java.lang.String MR_VERSION = "rotd-2023-03-31";
+    public static final java.lang.String MR_FULLARCH = "x86_64-pc-linux-gnu";
+}
