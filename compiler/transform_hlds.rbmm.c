9f4a88abc9614abb373d5040d8d25c8aad09a167
Julien Fischer <juliensf@gmail.com>
2023-07-23 22:34:47 +1000

    Imported mercury-srcdist-rotd-2023-07-23

---
+++ compiler/transform_hlds.rbmm.c
@@ -0,0 +1,66 @@
+/*
+** Automatically generated from `rbmm.m'
+** by the Mercury compiler,
+** version rotd-2023-07-23
+** configured for x86_64-pc-linux-gnu.
+** Do not edit.
+**
+** The autoconfigured grade settings governing
+** the generation of this C file were
+**
+** TAG_BITS=2
+** UNBOXED_FLOAT=no
+** UNBOXED_INT64S=no
+** PREGENERATED_DIST=yes
+** HIGHLEVEL_CODE=yes
+**
+** END_OF_C_GRADE_INFO
+*/
+
+
+// :- module transform_hlds.rbmm.
+// :- implementation.
+
+/*
+INIT mercury__transform_hlds__rbmm__init
+ENDINIT
+*/
+
+#include "transform_hlds.rbmm.mih"
+
+
+#include "builtin.mih"
+#include "private_builtin.mih"
+#include "transform_hlds.mih"
+
+
+
+
+
+
+
+
+
+
+
+
+void mercury__transform_hlds__rbmm__init(void)
+{
+}
+
+void mercury__transform_hlds__rbmm__init_type_tables(void)
+{
+}
+
+void mercury__transform_hlds__rbmm__init_debugger(void)
+{
+	MR_fatal_error("debugger initialization in MLDS grade");
+}
+
+// Ensure everything is compiled with the same grade.
+const char *mercury__transform_hlds__rbmm__grade_check(void)
+{
+    return &MR_GRADE_VAR;
+}
+
+// :- end_module transform_hlds.rbmm.
