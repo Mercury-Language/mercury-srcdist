5068e04751d860ad3b0e9f45d506b8570c23959d
Julien Fischer <juliensf@gmail.com>
2023-09-02 23:57:18 +1000

    Imported mercury-srcdist-rotd-2023-09-02

---
+++ tests/valid_seq/ambig_high_level.m
@@ -0,0 +1,32 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+
+:- module ambig_high_level.
+
+:- interface.
+
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+
+% :- import_module ambig_confuse1.
+% :- import_module ambig_confuse2.
+:- import_module ambig_types_high_level.
+
+main(!IO) :-
+    X = foo,
+    ambig(confuse(X), X, X).
+
+:- pred ambig(pred(A, B)::in(pred(in, in) is det),
+    A::in, B::in) is det.
+
+ambig(Pred, X, Y) :-
+    Pred(X, Y).
+
+:- pred confuse(a::in, a::in, a::in) is det.
+confuse(_, _, _).
+
+:- end_module ambig_high_level.
