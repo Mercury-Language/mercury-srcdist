de41c6c649e90bfc72342c29efac07083b58eb79
Julien Fischer <juliensf@gmail.com>
2025-02-14 00:08:02 +1100

    Imported mercury-srcdist-rotd-2025-02-13

---
+++ compiler/analysis.c
@@ -0,0 +1,65 @@
+/*
+** Automatically generated from `analysis.m'
+** by the Mercury compiler,
+** version rotd-2025-02-13
+** configured for x86_64-pc-linux-gnu.
+** Do not edit.
+**
+** The autoconfigured grade settings governing
+** the generation of this C file were
+**
+** TAG_BITS=2
+** UNBOXED_FLOAT=no
+** UNBOXED_INT64S=no
+** PREGENERATED_DIST=yes
+** HIGHLEVEL_CODE=yes
+**
+** END_OF_C_GRADE_INFO
+*/
+
+
+// :- module analysis.
+// :- implementation.
+
+/*
+INIT mercury__analysis__init
+ENDINIT
+*/
+
+#include "analysis.mih"
+
+
+#include "builtin.mih"
+#include "private_builtin.mih"
+
+
+
+
+
+
+
+
+
+
+
+
+void mercury__analysis__init(void)
+{
+}
+
+void mercury__analysis__init_type_tables(void)
+{
+}
+
+void mercury__analysis__init_debugger(void)
+{
+	MR_fatal_error("debugger initialization in MLDS grade");
+}
+
+// Ensure everything is compiled with the same grade.
+const char *mercury__analysis__grade_check(void)
+{
+    return &MR_GRADE_VAR;
+}
+
+// :- end_module analysis.
