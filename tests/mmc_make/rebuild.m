f8c5b64fdbcaf253a7beace49616433e65c0e8a8
Julien Fischer <juliensf@gmail.com>
2025-07-25 23:58:24 +1000

    Imported mercury-srcdist-rotd-2025-07-25

---
+++ tests/mmc_make/rebuild.m
@@ -0,0 +1,18 @@
+%---------------------------------------------------------------------------%
+% vim: ts=4 sw=4 et ft=mercury
+%---------------------------------------------------------------------------%
+%
+% "Hello World" in Mercury.
+
+% This source file is hereby placed in the public domain.  -fjh (the author).
+
+:- module rebuild.
+:- interface.
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+
+main(!IO) :-
+    io.write_string("Hello, world\n", !IO).
